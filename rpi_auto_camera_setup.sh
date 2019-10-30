cdf7123fa9635d7b96f72e3f257e3e0ecdd693e9
Prehani <Prehani@users.noreply.github.com>
2019-10-30 16:58:23 -0500

    added header, a few style changes

--- rpi_auto_camera_setup.sh
+++ rpi_auto_camera_setup.sh
@@ -1,76 +1,84 @@
-#!/bin/bash bash 
-#Start of the script--begin with some trace/debugging options
+#!usr/bin/env bash 
+
+#************************************************#
+#      	   rpi4_auto_camera_setup.sh             #
+#           written by Peter Rehani              #
+#               October 30, 2019                 #
+#                                                #
+#             Automatic RPI4 setup.              #
+#************************************************#
+
+
+# Start of the script--begin with some trace/debugging options
 set -o nounset
 set -o errexit
 
-#We are currently on raspbian buster, but just to check
-#If this changes in the future, just update 
+# We are currently on raspbian buster, but just to check
+# If this changes in the future, just update 
 cat /etc/os-release
 os = "buster"
 
-#Initial user loading
+# Initial user loading
 init_setup(){
-	#Echo statement
+	# Echo statement
 	echo "Raspberry Pi Camera setup!"
-	#Make sure everything is up to dater 
+	# Make sure everything is up to dater 
 	sudo apt-get update && sudo apt-get upgrade                                                                                                
 
 	if grep 'opt' /etc/passwd; then
-	    #Nothing needs to be done
+	    # Nothing needs to be done
 		echo "opt user already exists!"
 	else
-		#I think the password setup here is incorrect? 
-		#Security is not great here as I would need to type the process
-		#in the PID which could create issues/allow other users to see the 
-		#password; this isn't relevant now, but something to keep in mind
+		# I think the password setup here is incorrect? 
+		# Security is not great here as I would need to type the process
+		# in the PID which could create issues/allow other users to see the 
+		# password; this isn't relevant now, but something to keep in mind
 		sudo adduser opt --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
-		#sudo chpasswd << 'END'
+		# sudo chpasswd << 'END'
 		opt:'OPTproject2019'
 	fi
 }
 
-#Miniconda install
+# Miniconda install
 miniconda_install(){ 
-	#Begin with a miniconda installation: we need to download the latest
-	#version of miniconda as well as check the md5sum and then execute the 
-	#install script for the miniconda installer
+	# Begin with a miniconda installation: we need to download the latest
+	# version of miniconda as well as check the md5sum and then execute the 
+	# install script for the miniconda installer
 
 	echo "Downloading and installing miniconda"
 	wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
 	sudo md5sum Miniconda3-latest-Linux-armv7l.sh
 	sudo /bin/bash Miniconda3-latest-Linux-armv7l.sh
 	
-	#Next, add conda to the path to ensure that everything runs smoothly, and 
-	#so that we can execute conda commands with ease:
+	# Next, add conda to the path to ensure that everything runs smoothly, and 
+	# so that we can execute conda commands with ease:
 	echo "Adding miniconda to the path and bashrc" 
-	#This is wrong, need to append a line to the end 
+	# This is wrong, need to append a line to the end 
 	sudo nano /home/pi/.bashrc
 	export PATH="/home/pi/miniconda3/bin:$PATH" 
-	conda
 }
 
-#Miniconda setup 
+# Miniconda setup 
 miniconda_setup(){
-	#Next, add appropriate channels to install the correct version of python
-	conda 
-	if $? !=0; then 
+	# Next, add appropriate channels to install the correct version of python
+	if  ! type "$conda" > /dev/null; then 
 		miniconda_install
 	fi 
 	echo "Adding appropriate channels for rpi:"
 	conda config --add channels rpi
-	#And create/activate the appropriate environments 
+	# And create/activate the appropriate environments 
 	echo "Creating and activating opt environment"
-	#If opt environment is in the list of conda environments, pass effectively 
+	# If opt environment is in the list of conda environments, pass effectively 
 	if conda info --envs | grep 'opt'; then 
 		echo "Environment opt already exists!"
-	#Otherwise, continue 
+	# Otherwise, continue 
 	else 
 		conda create --name opt python=3.7 -y 
 	fi	
-	#Activate the new environment 
+	# Activate the new environment 
 	source activate opt
 	
-	#Lastly, call the conda setup script to install the appropriate packages
+	# Lastly, call the conda setup script to install the appropriate packages
 	echo "Install script for appropriate python packages"
 	declare -a python_packages=("numpy" "matplotlib" "scipy" "scipy" "scikit-image" "imagecodecs-lite" "numba" "h5py" "pyqtgraph")
 	for i in "${python_packages[@]}"
@@ -82,14 +90,14 @@ miniconda_setup(){
 	conda install -c conda-forge tifffile 
 }
 
-#ROS setup per http://wiki.ros.org/ROSberryPi/Installing%20ROS%20Kinetic%20on%20the%20Raspberry%20Pi
+# ROS setup per http://wiki.ros.org/ROSberryPi/Installing%20ROS%20Kinetic%20on%20the%20Raspberry%20Pi
 ros_setup(){ 
 	echo "2.1 Setup ROS Repos"
 	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 	
 	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key \
 		 C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-	#Ensure everything is up to date	
+	# Ensure everything is up to date	
 	sudo apt-get update && sudo apt-get upgrade -y 
 	echo "2.2 Install Boostrap Dependencies" 
 	sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential cmake
@@ -105,7 +113,7 @@ ros_setup(){
 	rosinstall_generator ros_comm --rosdistro kinetic --deps --wet-only --tar > kinetic-ros_comm-wet.rosinstall 
 	wstool init src kinetic-ros_comm-wet.rosinstall
 
-	#Effectively try the install; if it fails, update and try again
+	# Effectively try the install; if it fails, update and try again
 	rosinstall_generator robot --rosdistro kinetic --deps --wet-only --tar > kinetic-robot-wet.rosinstall\
 	&& wstool init src kinetic-robot-wet.rosinstall 
 	if [[ $? -ne 0 ]]; then 
@@ -129,7 +137,7 @@ ros_setup(){
 	rosinstall_generator desktop --rosdistro kinetic --deps --wet-only --exclude collada_parser collada_urdf --tar > kinetic-desktop-wet.rosinstall
 
 	cd ~/ros_catkin_ws
-	rosdep install -y --from-paths src --ignore-src --rosdistro kinetic -r --os=debian:${os}
+	rosdep install -y --from-paths src --ignore-src --rosdistro kinetic -r --os=debian:"$os"
 
 	sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/kinetic
 
