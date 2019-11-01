46955717ed7f20f132c6426824cfe7599f355eea
Prehani <Prehani@users.noreply.github.com>
2019-11-01 12:29:25 -0500

    fixed miniconda install

--- rpi_auto_camera_setup.sh
+++ rpi_auto_camera_setup.sh
@@ -23,7 +23,7 @@ init_setup(){
 	# Echo statement
 	echo "Raspberry Pi Camera setup!"
 	# Make sure everything is up to dater 
-	sudo apt-get update && sudo apt-get upgrade                                                                                                
+	sudo apt-get update && sudo apt-get upgrade -y                                                                                             
 
 	if grep 'opt' /etc/passwd; then
 	    # Nothing needs to be done
@@ -48,13 +48,13 @@ miniconda_install(){
 	echo "Downloading and installing miniconda"
 	wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
 	sudo md5sum Miniconda3-latest-Linux-armv7l.sh
-	sudo /bin/bash Miniconda3-latest-Linux-armv7l.sh
-	
+	sudo /bin/bash Miniconda3-latest-Linux-armv7l.sh -b -p "/home/pi/miniconda3" 
 	# Next, add conda to the path to ensure that everything runs smoothly, and 
 	# so that we can execute conda commands with ease:
 	echo "Adding miniconda to the path and bashrc" 
 	# This is wrong, need to append a line to the end 
-	sudo nano /home/pi/.bashrc
+	sudo echo "source /home/pi/miniconda3/bin" >> ~/.bashrc 
+	source ~/.bashrc
 	export PATH="/home/pi/miniconda3/bin:$PATH" 
 }
 
@@ -66,6 +66,7 @@ miniconda_setup(){
 	fi 
 	echo "Adding appropriate channels for rpi:"
 	conda config --add channels rpi
+	sudo chown -R pi:pi anaconda3
 	# And create/activate the appropriate environments 
 	echo "Creating and activating opt environment"
 	# If opt environment is in the list of conda environments, pass effectively 
@@ -73,21 +74,20 @@ miniconda_setup(){
 		echo "Environment opt already exists!"
 	# Otherwise, continue 
 	else 
-		conda create --name opt python=3.7 -y 
+		conda install python=3.6
+		conda create --name opt python=3.6 -y 
 	fi	
 	# Activate the new environment 
 	source activate opt
 	
 	# Lastly, call the conda setup script to install the appropriate packages
 	echo "Install script for appropriate python packages"
-	declare -a python_packages=("numpy" "matplotlib" "scipy" "scipy" "scikit-image" "imagecodecs-lite" "numba" "h5py" "pyqtgraph")
-	for i in "${python_packages[@]}"
+	declare -a python_packages=("numpy" "matplotlib" "scipy" "scipy" "scikit-image" "imagecodecs-lite" "numba" "h5py" "pyqtgraph" "tifffile")
+	for i in "${python_packages[@]}" 
 	do
-		echo "installing" $i 
-		conda install $i -y  
+		echo "installing" $i
+		conda install -c conda-forge $i -y  
 	done
-	echo "Install tifffile"
-	conda install -c conda-forge tifffile 
 }
 
 # ROS setup per http://wiki.ros.org/ROSberryPi/Installing%20ROS%20Kinetic%20on%20the%20Raspberry%20Pi
@@ -117,35 +117,52 @@ ros_setup(){
 	rosinstall_generator robot --rosdistro kinetic --deps --wet-only --tar > kinetic-robot-wet.rosinstall\
 	&& wstool init src kinetic-robot-wet.rosinstall 
 	if [[ $? -ne 0 ]]; then 
-		wstool update -j4 -t src &&\
-		rosinstall_generator robot --rosdistro kinetic --deps --wet-only --tar > kinetic-robot-wet.rosinstall &&\
-		wstool init src kinetic-robot-wet.rosinstall
+		wstool update -j4 -t src \
+		&& rosinstall_generator robot --rosdistro kinetic --deps --wet-only --tar > kinetic-robot-wet.rosinstall \
+		&& wstool init src kinetic-robot-wet.rosinstall
 	fi 
 
 	echo "3.2 Resolve Dependencies"
 	sudo apt-get install cmake -y 
 
-	mkdir -p ~/ros_catkin_ws/external_src
-	cd ~/ros_catkin_ws/external_src
-	wget http://sourceforge.net/projects/assimp/files/assimp-3.1/assimp-3.1.1_no_test_models.zip/download -O assimp-3.1.1_no_test_models.zip
-	unzip assimp-3.1.1_no_test_models.zip
-	cd assimp-3.1.1
-	cmake .
-	make
-	sudo make install
+	echo "3.2.1 Fixing Known Unavailable Dependencies"
+	(mkdir -p ~/ros_catkin_ws/external_src\
+	&& cd ~/ros_catkin_ws/external_src
+	&& wget http://sourceforge.net/projects/assimp/files/assimp-3.1/assimp-3.1.1_no_test_models.zip/download -O assimp-3.1.1_no_test_models.zip\
+	&& unzip assimp-3.1.1_no_test_models.zip\
+	&& cd assimp-3.1.1\
+	&& cmake . \
+	&& make \
+	&& sudo make install)
+	|| (echo "Issue with first install; trying alternative ros generator to skip collada_urdf"\
+	&& rosinstall_generator desktop --rosdistro kinetic --deps --wet-only --exclude collada_parser collada_urdf --tar > kinetic-desktop-wet.rosinstall)
 
-	rosinstall_generator desktop --rosdistro kinetic --deps --wet-only --exclude collada_parser collada_urdf --tar > kinetic-desktop-wet.rosinstall
-
+	echo "3.2.2 Finish Resolving Remainder of Dependencies"
 	cd ~/ros_catkin_ws
 	rosdep install -y --from-paths src --ignore-src --rosdistro kinetic -r --os=debian:"$os"
 
 	sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/kinetic
 
+	# At this point, we should be ready for catkin workspace setup 
+	echo "3.3 Building the catkin Workspace"
+
+	sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/kinetic
+	
+	# If above fails, it's likely a memory exhaustion error with internal compiler error so retry 
+	if [[ $? -ne 0 ]]; then 
+		sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/kinetic -j2
+	fi  
+
+	# At this point, ROS should be installed
+	# Last step is to source and add to bashrc
+	source /opt/ros/kinetic/setup.bash 
+	echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+
+
 }
 
 init_setup
-miniconda_install
-miniconda_setup
+iniconda_setup
 ros_setup 
 
 exit $?
